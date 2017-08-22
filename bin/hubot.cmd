27ef3fc4fddf0381eff794bc7bc404728f17b9ff
HPham <tpham40@illinois.edu>
2017-08-21 21:34:42 -0500

    Changed hubot slack token

--- bin/hubot.cmd
+++ bin/hubot.cmd
@@ -4,4 +4,4 @@ call npm install
 SETLOCAL
 SET PATH=node_modules\.bin;node_modules\hubot\node_modules\.bin;%PATH%
 
-node_modules\.bin\hubot.cmd --name "testbot" %* 
+node_modules\.bin\hubot.cmd --name "vietbot" %* 
